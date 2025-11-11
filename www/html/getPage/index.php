79c32d9fc5341835626542f1138246ccd3738385
Schorsch3000 <schorsch3000@shnbk.de>
2025-11-11 15:44:44 +0100

    show files

--- www/html/getPage/index.php
+++ www/html/getPage/index.php
@@ -6,8 +6,8 @@ $hosts = glob('*', GLOB_ONLYDIR);
 natsort($hosts);
 
 foreach ($hosts as $host) {
-    echo "Host: $host\n\n";
-    $services = glob("$host/*.path", GLOB_ONLYDIR);
+    echo "# Host: $host\n\n";
+    $services = glob("$host/*.path");
     natsort($services);
     foreach ($services as $servicePathFile) {
         $service = basename(dirname(trim(file_get_contents($servicePathFile))));
