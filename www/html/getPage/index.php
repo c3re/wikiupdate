9dd39513fc0cc67623733b444b5c53bb587d36da
Schorsch3000 <schorsch3000@shnbk.de>
2025-11-12 13:48:21 +0100

    sort services by actual name

--- www/html/getPage/index.php
+++ www/html/getPage/index.php
@@ -8,9 +8,17 @@ $hosts = glob('*', GLOB_ONLYDIR);
 natsort($hosts);
 
 foreach ($hosts as $host) {
+    if (filemtime($host) < time() - 7 * 24 * 60 * 60) {
+        // skip hosts not modified in the last 7 days
+        continue;
+    }
     echo "# Host: $host\n\n";
     $services = glob("$host/*.path");
-    natsort($services);
+
+    usort($services, function ($a, $b) {
+        return strnatcasecmp(file_get_contents($a), file_get_contents($b));
+    });
+
     foreach ($services as $servicePathFile) {
         $service = basename(dirname(trim(file_get_contents($servicePathFile))));
         echo " - [$service](./Dienste-Intern/$service)\n";
